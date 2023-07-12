10e4cecf426631602f2c23a9ba2afe4fbf7c0c7e
K9D3U2 <kdng8047@naver.com>
2023-07-12 17:21:02 +0900

    brh1 230712 17:20

--- src/main/java/customer/controller/MyDispatcher.java
+++ src/main/java/customer/controller/MyDispatcher.java
@@ -3,9 +3,41 @@ package customer.controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MyDispatcher {
+import customer.controller.notice.NoticeController;
+
+public class MyDispatcher extends HttpServlet {
+	private Controller controller;
 	
+	public MyDispatcher() {
+		controller=null;
+	}
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri=request.getRequestURI();
+		String ctxPath=request.getContextPath();
+		String com=uri.substring(ctxPath.length());
+		
+		System.out.println(com);	//	확인용
+		
+		switch(com) {
+		case "/customer/notice.do":
+			controller=new NoticeController();
+			break;
+		case "/customer/noticeReg.do":
+			controller=new NoticeController();
+			break;
+		}
+		
+		try {
+			controller.execute(request, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/customer/dao/NoticeDao.java
+++ src/main/java/customer/dao/NoticeDao.java
@@ -1,14 +1,38 @@
 package customer.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
+import customer.db.DBCon;
 import customer.vo.Notice;
 
 public class NoticeDao {
 
-	public List<Notice> noticeSelAll(String field, String query) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Notice> noticeSelAll(String field, String query) throws Exception {
+		Connection con = DBCon.getConnection();
+		String sql = "select * from notices where " + field + " like ?";
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		pstmt.setString(1, "%" + query + "%");
+		ResultSet rs = pstmt.executeQuery();
+		List<Notice> list = new ArrayList<>();
+
+		while (rs.next()) {
+			Notice n = new Notice();
+
+			n.setSeq(rs.getString("seq"));
+			n.setTitle(rs.getString("title"));
+			n.setWriter(rs.getString("writer"));
+			n.setContent(rs.getString("content"));
+			n.setRegdate(rs.getDate("regdate"));
+			n.setHit(rs.getInt("hit"));
+
+			list.add(n);
+		}
+
+		return list;
 	}
 
 	public Notice getNotice(String seq, String hit) {
