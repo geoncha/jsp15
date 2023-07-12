e60c691482a5b6e20a29920a02a9e3bb24b4007e
geoncha <ckwhdrjs77@naver.com>
2023-07-12 16:46:18 +0900

    m1

---
+++ src/main/java/customer/dao/NoticeDao.java
@@ -0,0 +1,19 @@
+package customer.dao;
+
+import java.util.List;
+
+import customer.vo.Notice;
+
+public class NoticeDao {
+
+	public List<Notice> noticeSelAll(String field, String query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Notice getNotice(String seq, String hit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
